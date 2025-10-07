0c4d8072113f4db0f6df23f4dc22049c6afbb7eb
Kevin Freccero <kevinfreccero5@gmail.com>
2025-10-06 22:19:41 -0300

    pruebas

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,18 +2,29 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class BebidaTest extends TestCase
 {
     /**
-     * A basic test example.
+     * A basic feature test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_ListarTodo(): void
     {
-        $response = $this->get('/');
-
+        $response = $this->get('/api/bebidas');
         $response->assertStatus(200);
+        $response->assertJsonStructure([
+            '*' => [
+                "id",
+                "name",
+                "lastname",
+                "phone",
+                "created_at",
+                "updated_at",
+                "deleted_at"
+            ]
+        ]);
     }
 }
